8b2b6e4e4c45c320eeed36db2aab8928fcd96ba8
Yuto Mabe <yuto96315mabe@gmail.com>
2025-12-12 20:33:44 +0900

    feat: firebase set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:talkenote/firebase_options.dart';
 import 'package:talkenote/ui/splash_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
