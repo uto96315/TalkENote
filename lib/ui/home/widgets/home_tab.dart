dd45ef8310aa84f94fbc8a0f7d6644da79c15ce6
Yuto Mabe <yuto96315mabe@gmail.com>
2025-12-18 02:30:35 +0900

    feat: 録音時のボタンデザインを編集

--- lib/ui/home/widgets/home_tab.dart
+++ lib/ui/home/widgets/home_tab.dart
@@ -81,7 +81,7 @@ class HomeTabPage extends ConsumerWidget {
   }
 }
 
-class _RecordButton extends StatelessWidget {
+class _RecordButton extends StatefulWidget {
   const _RecordButton({
     required this.isRecording,
     required this.onTap,
@@ -91,13 +91,74 @@ class _RecordButton extends StatelessWidget {
   final VoidCallback onTap;
 
   @override
+  State<_RecordButton> createState() => _RecordButtonState();
+}
+
+class _RecordButtonState extends State<_RecordButton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1600),
+    );
+    _maybeAnimate();
+  }
+
+  @override
+  void didUpdateWidget(covariant _RecordButton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.isRecording != widget.isRecording) {
+      _maybeAnimate();
+    }
+  }
+
+  void _maybeAnimate() {
+    if (widget.isRecording) {
+      _controller.repeat();
+    } else {
+      _controller.stop();
+      _controller.reset();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final baseColor = AppColors.primary;
     return GestureDetector(
-      onTap: onTap,
+      onTap: widget.onTap,
       child: Stack(
         alignment: Alignment.center,
         children: [
+          if (widget.isRecording) ...[
+            _Pulse(
+              controller: _controller,
+              beginScale: 1.1,
+              endScale: 1.8,
+              beginOpacity: 0.35,
+              endOpacity: 0.0,
+              color: baseColor,
+              delay: 0.0,
+            ),
+            _Pulse(
+              controller: _controller,
+              beginScale: 1.3,
+              endScale: 2.0,
+              beginOpacity: 0.28,
+              endOpacity: 0.0,
+              color: baseColor,
+              delay: 0.3,
+            ),
+          ],
           AnimatedContainer(
             duration: const Duration(milliseconds: 300),
             width: 220,
@@ -112,9 +173,10 @@ class _RecordButton extends StatelessWidget {
               ),
               boxShadow: [
                 BoxShadow(
-                  color: baseColor.withOpacity(isRecording ? 0.45 : 0.3),
-                  blurRadius: isRecording ? 30 : 18,
-                  spreadRadius: isRecording ? 8 : 4,
+                  color:
+                      baseColor.withOpacity(widget.isRecording ? 0.45 : 0.3),
+                  blurRadius: widget.isRecording ? 30 : 18,
+                  spreadRadius: widget.isRecording ? 8 : 4,
                 ),
               ],
             ),
@@ -125,7 +187,7 @@ class _RecordButton extends StatelessWidget {
             height: 150,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
-              color: isRecording ? Colors.white : baseColor,
+              color: widget.isRecording ? Colors.white : baseColor,
               boxShadow: [
                 BoxShadow(
                   color: baseColor.withOpacity(0.35),
@@ -136,14 +198,62 @@ class _RecordButton extends StatelessWidget {
             ),
             child: Center(
               child: Icon(
-                isRecording ? Icons.stop_rounded : Icons.mic_rounded,
+                widget.isRecording ? Icons.stop_rounded : Icons.mic_rounded,
                 size: 48,
-                color: isRecording ? baseColor : Colors.white,
+                color: widget.isRecording ? baseColor : Colors.white,
               ),
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class _Pulse extends StatelessWidget {
+  const _Pulse({
+    required this.controller,
+    required this.beginScale,
+    required this.endScale,
+    required this.beginOpacity,
+    required this.endOpacity,
+    required this.color,
+    required this.delay,
+  });
+
+  final AnimationController controller;
+  final double beginScale;
+  final double endScale;
+  final double beginOpacity;
+  final double endOpacity;
+  final Color color;
+  final double delay;
+
+  @override
+  Widget build(BuildContext context) {
+    final curved = CurvedAnimation(
+      parent: controller,
+      curve: Interval(delay, 1.0, curve: Curves.easeOut),
+    );
+    final scale = Tween<double>(begin: beginScale, end: endScale).animate(curved);
+    final opacity =
+        Tween<double>(begin: beginOpacity, end: endOpacity).animate(curved);
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (_, __) {
+        return Transform.scale(
+          scale: scale.value,
+          child: Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: color.withOpacity(opacity.value),
+            ),
+          ),
+        );
+      },
     );
   }
 }
