228dc44e233eb9533d7a067c246462ec3a88d6d0
Yuto Mabe <yuto96315mabe@gmail.com>
2025-12-18 02:04:08 +0900

    色の修正

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -8,7 +8,7 @@ class AppColors {
   static const Color secondary = Color(0xFF4A90E2);
   static const Color background = Color(0xFFF5F7FB);
   static const Color textPrimary = Color(0xFF1F2430);
-  static const Color textSecondary = Color(0xFF4B5563);
+  static const Color textSecondary = Color.fromARGB(255, 132, 137, 145);
   static const Color success = Color(0xFF22C55E);
   static const Color warning = Color(0xFFF59E0B);
   static const Color error = Color(0xFFEF4444);
